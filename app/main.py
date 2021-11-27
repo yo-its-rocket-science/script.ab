bcaa8936bdfaca89d18e6e899c8d86a26d4ec4ae
awave1 <artgolovin96@gmail.com>
2021-11-27 03:28:24 -0700

    wip

--- app/main.py
+++ app/main.py
@@ -15,17 +15,3 @@ app.register_blueprint(company.company_routes, url_prefix='/company')
 app.register_blueprint(application.application_routes,
                        url_prefix='/application')
 app.register_blueprint(skill.skill_routes, url_prefix='/skill')
-
-
-@app.route("/")
-def hello():
-    return "yo"
-
-
-@app.route("/users", methods=["GET"])
-def get_users():
-    users = {
-        "users": [{"name": "John", "age": 30}],
-    }
-
-    return users
--- app/routes/candidate.py
+++ app/routes/candidate.py
@@ -1,4 +1,4 @@
 from flask import Blueprint
 from app.controllers import candidate_controller
 
-candidate_routes = Blueprint("user_routes", __name__)
+candidate_routes = Blueprint("candidate_routes", __name__)
